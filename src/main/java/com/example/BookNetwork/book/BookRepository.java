249942bd63fc67c2266fe875b4be1aa77c74b4d6
carren chepkorir <carren.chepkorir@turnkeyafrica.com>
2025-01-09 18:57:27 +0300

    Added ability to add and retrieve all books

--- src/main/java/com/example/BookNetwork/book/BookRepository.java
+++ src/main/java/com/example/BookNetwork/book/BookRepository.java
@@ -1,8 +1,19 @@
 package com.example.BookNetwork.book;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.math.BigDecimal;
 
 public interface BookRepository  extends JpaRepository<Book,BigDecimal> {
+@Query(
+        "SELECT book\n" +
+                "            FROM Book book\n" +
+                "            WHERE book.archived = false\n" +
+                "            AND book.sharable = true\n" +
+                "            AND book.owner.id ! =:userId"
+)
+    Page<Book> findAllDisplayableBooks(Pageable pageable, BigDecimal userId);
 }
